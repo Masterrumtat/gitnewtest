066de690a3636cee9da87fb53bb01523adfa9517
Masterrumtat <captain019377@gmail.com>
2023-11-10 23:51:20 +0700

    Initial Commit

---
+++ gitTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest1
+//
+//  Created by Vituruch Sinthusate on 10/11/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
